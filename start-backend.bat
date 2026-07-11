870779e92abfc15e6f3ee36506de60128b78d582
Rahul  Gupta <rahul_ua2503cdh466@iitp.ac.in>
2026-07-11 19:29:30 +0530

    removed extra file logs

--- start-backend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-echo Starting SentinelAI backend on http://127.0.0.1:8000
-echo MongoDB URL: %MONGODB_URL%
-python -m uvicorn backend.main:app --host 127.0.0.1 --port 8000
-pause
